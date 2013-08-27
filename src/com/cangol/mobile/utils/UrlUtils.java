088384c43c43faa81571656b7a057cb997f2f9ce
xuewu wei <wxw404@gmail.com>
2013-08-27 21:20:41 +0800

    add LICENSE

--- src/com/cangol/mobile/utils/UrlUtils.java
+++ src/com/cangol/mobile/utils/UrlUtils.java
@@ -1,5 +1,5 @@
 /** 
- * Copyright (c) 2013 xuewu.wei.
+ * Copyright (c) 2013 Cangol
  * 
  * Licensed under the Apache License, Version 2.0 (the "License")
  * you may not use this file except in compliance with the License.
