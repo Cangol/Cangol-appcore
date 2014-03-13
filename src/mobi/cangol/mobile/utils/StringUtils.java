535f2f622a22a964dbfc95178ac1e0fdd74bfcc8
Cangol <wxw404@gmail.com>
2014-03-13 09:47:17 +0800

    update

--- src/mobi/cangol/mobile/utils/StringUtils.java
+++ src/mobi/cangol/mobile/utils/StringUtils.java
@@ -422,4 +422,44 @@ public class StringUtils {
         }
         return true;
     }
+    
+	public static byte[] byteXorInt(byte[] data,int xor){
+		byte[] data2=new byte[4];
+		for(int i=0;i<data.length;i=i+4){
+			data2[0]=data[i/4+0];
+			data2[1]=data[i/4+1];
+			data2[2]=data[i/4+2];
+			data2[3]=data[i/4+3];
+			data2=int2byteArray(byteArray2int(data2)^xor);
+			data[i/4+0]=data2[0];
+			data[i/4+1]=data2[1];
+			data[i/4+2]=data2[2];
+			data[i/4+3]=data2[3];
+		}
+		return data;
+	}
+	public static int byteArray2int(byte[] b){
+	    byte[] a = new byte[4];
+	    int i = a.length - 1,j = b.length - 1;
+	    for (; i >= 0; i--,j--) {//从b的尾部(即int值的低位)开始copy数据
+	        if(j >= 0)
+	            a[i] = b[j];
+	        else
+	            a[i] = 0;//如果b.length不足4,则将高位补0
+	  }
+	    int v0 = (a[0] & 0xff) << 24;//&0xff将byte值无差异转成int,避免Java自动类型提升后,会保留高位的符号位
+	    int v1 = (a[1] & 0xff) << 16;
+	    int v2 = (a[2] & 0xff) << 8;
+	    int v3 = (a[3] & 0xff) ;
+	    return v0 + v1 + v2 + v3;
+	 }
+	
+	public static byte[] int2byteArray(int num) {
+		   byte[] result = new byte[4];
+		   result[0] = (byte)(num >>> 24);
+		   result[1] = (byte)(num >>> 16);
+		   result[2] = (byte)(num >>> 8); 
+		   result[3] = (byte)(num );      
+		   return result;
+	}
 }
