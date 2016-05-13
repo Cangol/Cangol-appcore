8ba2dbbb2872a78e5364c250b26b82f18777d8fb
Cangol <wxw404@gmail.com>
2016-05-13 15:39:11 +0800

    add parser demo

--- appcore-demo/src/main/java/mobi/cangol/mobile/appcore/demo/ParserFragment.java
+++ appcore-demo/src/main/java/mobi/cangol/mobile/appcore/demo/ParserFragment.java
@@ -7,10 +7,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONObject;
+
+import mobi.cangol.mobile.logging.Log;
+import mobi.cangol.mobile.parser.JSONParserException;
+import mobi.cangol.mobile.parser.JsonUtils;
+import mobi.cangol.mobile.utils.TimeUtils;
+
 /**
  * Created by weixuewu on 16/4/30.
  */
 public class ParserFragment extends Fragment {
+    private static final String TAG="JsonUtils";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,5 +33,25 @@ public class ParserFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        test();
+    }
+
+    public void test(){
+
+        Chat chat=new Chat();
+        chat.setUid("100001");
+        chat.setNickname("Cangol");
+        chat.setContent("pleas test ");
+        chat.setTime(TimeUtils.getCurrentTime());
+        Log.d(TAG,"old chat="+chat);
+        JSONObject json=JsonUtils.toJSONObject(chat);
+        Log.d(TAG,"old chatJson="+json);
+        Chat chatNew=null;
+        try {
+             chatNew=JsonUtils.parserToObject(Chat.class,json.toString());
+        } catch (JSONParserException e) {
+            e.printStackTrace();
+        }
+        Log.d(TAG,"new chat="+chatNew);
     }
 }
