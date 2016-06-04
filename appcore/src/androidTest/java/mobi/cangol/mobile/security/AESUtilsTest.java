4f1abd066e0b4eb4c67dac019392b8345ad4c55f
Cangol <wxw404@gmail.com>
2016-06-05 00:25:05 +0800

    add test demo

---
+++ appcore/src/androidTest/java/mobi/cangol/mobile/security/AESUtilsTest.java
@@ -0,0 +1,25 @@
+package mobi.cangol.mobile.security;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by weixuewu on 16/6/4.
+ */
+public class AESUtilsTest extends InstrumentationTestCase {
+
+    public void testEncrypt() throws Exception {
+
+    }
+
+    public void testDecrypt() throws Exception {
+
+    }
+
+    public void testToByte() throws Exception {
+
+    }
+
+    public void testToHex() throws Exception {
+
+    }
+}
