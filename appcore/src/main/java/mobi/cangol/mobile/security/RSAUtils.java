772d0bc126407f511c0b34a60ba4c3d1162afa6c
Cangol <wxw404@gmail.com>
2016-06-08 17:40:12 +0800

    testCase

--- appcore/src/main/java/mobi/cangol/mobile/security/RSAUtils.java
+++ appcore/src/main/java/mobi/cangol/mobile/security/RSAUtils.java
@@ -238,34 +238,4 @@ public class RSAUtils {
         }
         return arrays;
     }
-
-    public static void main(String[] args) throws Exception {
-        // TODO Auto-generated method stub   
-        HashMap<String, Object> map = RSAUtils.getKeys();
-        //生成公钥和私钥   
-        RSAPublicKey publicKey = (RSAPublicKey) map.get("public");
-        RSAPrivateKey privateKey = (RSAPrivateKey) map.get("private");
-
-        //模   
-        String modulus = publicKey.getModulus().toString();
-        System.err.println("模 " + modulus);
-        //公钥指数   
-        String public_exponent = publicKey.getPublicExponent().toString();
-        System.err.println("公钥指数 " + public_exponent);
-        //私钥指数   
-        String private_exponent = privateKey.getPrivateExponent().toString();
-        System.err.println("私钥指数 " + private_exponent);
-        //明文   
-        String ming = "123456789";
-        System.err.println("明文 " + ming);
-        //使用模和指数生成公钥和私钥   
-        RSAPublicKey pubKey = RSAUtils.getPublicKey(modulus, public_exponent);
-        RSAPrivateKey priKey = RSAUtils.getPrivateKey(modulus, private_exponent);
-        //加密后的密文   
-        String mi = RSAUtils.encryptByPublicKey(ming, pubKey);
-        System.err.println("密文 " + mi);
-        //解密后的明文   
-        ming = RSAUtils.decryptByPrivateKey(mi, priKey);
-        System.err.println("原文 " + ming);
-    }
 }
