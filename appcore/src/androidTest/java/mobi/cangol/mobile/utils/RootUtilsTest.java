28aef00afca48f19ac3629266663e3d3d24c4544
Cangol <wxw404@gmail.com>
2016-06-12 20:51:22 +0800

    update RootUtils  Test

---
+++ appcore/src/androidTest/java/mobi/cangol/mobile/utils/RootUtilsTest.java
@@ -0,0 +1,25 @@
+package mobi.cangol.mobile.utils;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by weixuewu on 16/6/12.
+ */
+public class RootUtilsTest extends AndroidTestCase {
+
+    public void testIsDeviceRooted() throws Exception {
+        RootUtils.isDeviceRooted();
+    }
+
+    public void testCheckRootMethod1() throws Exception {
+        RootUtils.checkRootMethod1();
+    }
+
+    public void testCheckRootMethod2() throws Exception {
+        RootUtils.checkRootMethod2();
+    }
+
+    public void testCheckRootMethod3() throws Exception {
+        RootUtils.checkRootMethod3();
+    }
+}
