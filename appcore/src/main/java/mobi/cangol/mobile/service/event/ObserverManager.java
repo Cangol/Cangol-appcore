3caa1fc444a71d06cf1fe2110b45492eddb48865
Cangol <wxw404@gmail.com>
2016-08-03 17:51:41 +0800

    add note

--- appcore/src/main/java/mobi/cangol/mobile/service/event/ObserverManager.java
+++ appcore/src/main/java/mobi/cangol/mobile/service/event/ObserverManager.java
@@ -23,11 +23,23 @@ import mobi.cangol.mobile.service.AppService;
  * Created by xuewu.wei on 2016/8/3.
  */
 public interface ObserverManager extends AppService {
-
+    /**
+     * 注册订阅者
+     * @param subscriber
+     */
     void register(Object subscriber);
 
+    /**
+     * 解除订阅者
+     * @param subscriber
+     */
     void unregister(Object subscriber);
 
+    /**
+     * 触发一个事件
+     * @param event 事件名称
+     * @param data 事件数据
+     */
     void post(String event, Object data);
 
 }
