0a4e6855add34d71525bf1b8bae853ec156cdc01
xuewu.wei <xuewu.wei@fdzqsh.cn>
2018-05-08 13:45:56 +0800

    u

--- appcore-demo/src/main/java/mobi/cangol/mobile/appcore/demo/MainActivity.java
+++ appcore-demo/src/main/java/mobi/cangol/mobile/appcore/demo/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends FragmentActivity {
         }
         Log.d("getMD5Fingerprint="+DeviceInfo.getMD5Fingerprint(this));
         Log.d("getSHA1Fingerprint="+DeviceInfo.getSHA1Fingerprint(this));
-
-
     }
 
     @Override
